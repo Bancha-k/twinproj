31f41d21f42aaa1e7fb54333886b0e7b2efb4eda
rotoon <rotoon.np@gmail.com>
2018-09-09 00:10:51 +0700

    catch eror duplicate

--- client/src/components/FormInput.js
+++ client/src/components/FormInput.js
@@ -37,25 +37,32 @@ class FormInput extends React.Component {
                       age: values.age,
                       favoriteTeam: values.favoriteTeam
                     }
-                  }).then(data => {
-                    if (data.data.addProfile == null) {
-                      this.setState({ fullplayer: true })
+                  })
+                    .then(data => {
+                      if (data.data.addProfile == null) {
+                        this.setState({ fullplayer: true })
+                        notification['error']({
+                          message:
+                            'Stadium Unavaliable!, Please Select other Stadium and Time!',
+                          description: 'Cannot Match'
+                        })
+                        // this.props.form.resetFields()
+                      } else {
+                        const result = dataAnalyte(data.data.addProfile)
+                        action(result)
+                        notification['success']({
+                          message: 'Successfully Added',
+                          description: `${values.fullname}`
+                        })
+                        // this.props.form.resetFields()
+                      }
+                    })
+                    .catch(() => {
                       notification['error']({
-                        message:
-                          'Stadium Unavaliable!, Please Select other Stadium and Time!',
+                        message: 'This Name is already exits',
                         description: 'Cannot Match'
                       })
-                      // this.props.form.resetFields()
-                    } else {
-                      const result = dataAnalyte(data.data.addProfile)
-                      action(result)
-                      notification['success']({
-                        message: 'Successfully Adding',
-                        description: `${values.fullname}`
-                      })
-                      // this.props.form.resetFields()
-                    }
-                  })
+                    })
                 }
               })
             }}
